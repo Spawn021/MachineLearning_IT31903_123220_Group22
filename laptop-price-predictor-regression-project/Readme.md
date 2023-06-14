bf51fd07da36ecc489e1ba2da24c708c6eb5c992
Spawn021 <tranquangnam27012002@gmail.com>
2023-06-14 23:30:46 +0700

    Up

---
+++ laptop-price-predictor-regression-project/Readme.md
@@ -0,0 +1,56 @@
+# IT31903_123220_Group22_Laptop_price_prediction
+
+[toc]
+
+# About the project
+
+In this project, we’re building a model capable of predicting price of laptop. The prediction is made by learning from thousands of laptops. Each laptop’s information includes RAM, CPU, GPU, memory, screen size and resolution, type name, operating system, and laptop brand (business), etc.
+
+Dataset source: [Kaggle](https://www.kaggle.com/code/danielbethell/laptop-prices-prediction/input?select=laptop_price.csv)
+
+# Requirement
+
+* Windows, Linux or Mac OS
+* Jupyter notebook
+* Python 3.6 or later
+* pip
+* git
+
+To install Python, see guide [here](https://www.python.org/downloads/).
+
+You will need an environment to run Jupyter notebook. See installation for your system [here](https://jupyter.org/install).
+
+To install pip, see guide [here](https://pip.pypa.io/en/stable/installing/).
+
+To install git, see guide [here](https://git-scm.com/downloads).
+
+## Installation
+
+Instead of building everything from scratch, we use these following libraries. So please install them before use any code.
+
+* Numpy, see [intallation](https://numpy.org/install/)
+* Pandas, see [intallation](https://pandas.pydata.org/pandas-docs/stable/getting_started/install.html)
+* Scikit-learn [intallation](https://scikit-learn.org/stable/install.html)
+* Imbalanced-learn [intallation](https://pypi.org/project/imbalanced-learn/)
+* Matplotlib [intallation](https://matplotlib.org/stable/users/installing.html)
+* Seaborn [intallation](https://seaborn.pydata.org/installing.html)
+* Pickle [intallation](https://pypi.org/project/pickle-mixin/)
+* Streamlit [installation] (https://docs.streamlit.io/library/get-started/installation)
+
+# Download project source code
+
+```
+https://github.com/Spawn021/MachineLearning_IT31903_123220_Group22
+```
+
+# Run web
+To implement prediction web, run:
+streamlit run app.py
+
+# Contributors
+
+* Tran Quang Nam - 20200427
+* Vu Thuy Linh - 20205159
+
+
+
---
+++ laptop-price-predictor-regression-project/laptop-price-predictor-regression-project-main/app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Import the model
+pipe = pickle.load(open('pipe.pkl', 'rb'), encoding='latin1')
+pipe1 = pickle.load(open('pipe1.pkl', 'rb'), encoding='latin1')
+df = pickle.load(open('df.pkl', 'rb'), encoding='latin1')
+
+# Set page title and background color
+st.set_page_config(page_title="Laptop Predictor", page_icon="💻", layout="centered", initial_sidebar_state="expanded")
+st.markdown('<style>body{background-color: #f5f5f5;}</style>', unsafe_allow_html=True)
+
+# Page title
+st.title("Laptop Predictor")
+
+# Brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# Type of laptop
+type = st.selectbox('Type', df['TypeName'].unique())
+
+# RAM
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# Weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# Screen size
+screen_size = st.number_input('Screen Size')
+
+# Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
+
+# OS
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Convert input values to appropriate format
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
+
+    # Create query array
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
+    query = query.reshape(1, 12)
+
+    # Predict price
+    predicted_price = int(np.exp(pipe1.predict(query)[0]))
+
+    # Show predicted price
+    st.success("The predicted price of this configuration is $" + str(predicted_price))
